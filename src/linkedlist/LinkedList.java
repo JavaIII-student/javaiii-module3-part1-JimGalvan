8563390e11654f2f8674d476f5580d2b2a6cc017
jinux95 <jimmy-galvan@live.com.mx>
2020-07-25 11:56:38 -0700

    new changes

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -60,42 +60,102 @@ public class LinkedList<T> implements LinkedListInterface<T> {
 	@Override
 	public void add(T data, int index) throws IndexOutOfBoundsException {
 
-		// This is declared as -1 instead of 0 in order to follow list index convention
-		int elementNum = 0; // This equals to index 0
+		// Variable to check index of the current node
+		int elementNum = 0;
 
-		Node<T> currentNode = firstNode; // D
+		Node<T> currentNode = firstNode;
 
 		while (currentNode != null) {
 
 			elementNum++;
 
-//			if (elementNum == 1) {
-//				firstNode = new Node<T>(data, firstNode);
-//				return;
-//			}
-
-			 if (elementNum == index) {
-				
-//				System.out.println(currentNode.getData());
-				
-				currentNode = new Node<T>(data, currentNode);
-				currentNode.setData(data);
-				
-//				System.out.println(currentNode.getData());
-
-//				currentNode.setNext(new Node<T>(data, currentNode.getNext()));
-				
-//				return;
-
+			if (index == 1) {
+				firstNode = new Node<T>(data, firstNode);
+				return;
 			}
 
-			currentNode = currentNode.getNext(); // assign the reference to the following node, this case node 'B'
+			if (elementNum == index) {
+				currentNode.setNext(new Node<T>(data, currentNode.getNext()));
+				return;
+			}
+
+			// This allows to set a reference to the first node in the list after first
+			// iteration
+			if (elementNum > 1) {
+				currentNode = currentNode.getNext();
+			}
+
+			// Custom exception if an inputed index cannot reach a node's reference
+			if (currentNode.getNext() == null & index > elementNum + 1) {
+
+				System.err.println("Error: index: " + index + " is out of bounds");
+				throw new IndexOutOfBoundsException();
+
+			}
 		}
 	}
 
 	@Override
 	public T remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
+
+		// Variable to check index of the current node
+		int elementNum = 0;
+		Node<T> currentNode = firstNode;
+
+		while (currentNode != null) {
+			
+			elementNum++;
+			
+			if (index == 1 & elementNum == 1) {
+
+				firstNode.setNext(firstNode.getNext());
+
+				firstNode.setData(firstNode.getNext().getData());
+			}
+			
+			
+			if (index == 1 & currentNode.getNext() != null) {
+				
+				currentNode.setNext(currentNode.getNext());
+				currentNode.setData(currentNode.getNext().getData());
+			}
+			
+			currentNode = currentNode.getNext();
+
+		}
+
+		
+		// This part of code moves the items when you insert an elements that is not at
+		// first node
+		
+//		if (elementNum == index -1) {
+//			
+//			currentNode.setNext(currentNode.getNext().getNext());
+//			currentNode.getNext().setData(currentNode.getNext().getData());
+//		}
+		
+		
+		// The following code deletes the last element in the list
+
+		currentNode = firstNode; // Set current node to first node
+		
+		// This part counts the number of elements in the list
+		int numOfElements = 0;
+
+		while (currentNode != null) {
+			numOfElements++;
+			currentNode = currentNode.getNext();
+		}
+
+		currentNode = firstNode;
+
+		for (int i = 0; i < numOfElements - 2; i++) {
+			currentNode = currentNode.getNext();
+		}
+		
+		currentNode.setNext(null);
+		lastNode.setNext(currentNode);
+
 		return null;
 	}
 
--- test/linkedlist/LinkedListTest.java
+++ test/linkedlist/LinkedListTest.java
@@ -12,13 +12,12 @@ import org.junit.Test;
  *
  */
 public class LinkedListTest {
-	
-	
+
 	@Test
 	public void isEmptyTest() {
-	
+
 		LinkedList<String> list = new LinkedList<>();
-		
+
 		list.addFirst("Juan");
 		list.addFirst("John");
 		list.addFirst("Lennon");
@@ -31,18 +30,18 @@ public class LinkedListTest {
 
 	@Test
 	public void sizeTest() {
-		
+
 		LinkedList<String> list = new LinkedList<>();
 		list.addLast("Juan");
 		list.addLast("John");
 		list.addLast("Lennon");
 
 		assertEquals(3, list.size());
-		
+
 		list.addLast("Jimmy");
 		list.addLast("Galvan");
 //		list.print();
-		
+
 		assertEquals(5, list.size());
 	}
 
@@ -50,16 +49,16 @@ public class LinkedListTest {
 	public void addFirstTest() {
 
 		LinkedList<Integer> list = new LinkedList<>();
-		
+
 		// List uses class wrapper Integer, so use this type of object to test values
 		Integer number4 = 4;
 		Integer number1 = 1;
-		
+
 		list.addFirst(1);
 		list.addFirst(2);
 		list.addFirst(3);
 		list.addFirst(4);
-		
+
 		assertEquals(number4, list.firstNode.getData());
 		assertEquals(number1, list.lastNode.getData());
 	}
@@ -67,65 +66,83 @@ public class LinkedListTest {
 	@Test
 	public void addLastTest() {
 		LinkedList<Double> list = new LinkedList<>();
-		
+
 		Double double3 = 3.0;
-		
+
 		list.addLast(1.0);
 		list.addLast(2.0);
 		list.addLast(3.0);
-		
+
 		assertEquals(double3, list.lastNode.getData());
-		
+
 		LinkedList<String> list2 = new LinkedList<>();
-		
+
 		list2.addLast("Juan");
 		list2.addLast("Jim");
 		list2.addLast("Perez");
 		list2.addLast("Jimmy");
-		
+
 		assertEquals("Jimmy", list2.lastNode.getData());
 	}
 
 	@Test
 	public void addTest() throws IndexOutOfBoundsException {
 		LinkedList<Character> listCh = new LinkedList<>();
-		
+
 		listCh.addFirst('A');
 		listCh.addFirst('B');
 		listCh.addFirst('C');
 		listCh.addFirst('D');
-		
-		listCh.add('S', 1);
-		listCh.add('R', 2);	
 
-		listCh.print();
-		
+		listCh.add('S', 1);
+		listCh.add('R', 2);
+		listCh.add('E', 7);
+
 		Character S = 'S';
 		Character R = 'R';
-		
+		Character E = 'E';
+
 		assertEquals(S, listCh.firstNode.getData());
 		assertEquals(R, listCh.firstNode.getNext().getData());
-		
-
+		assertEquals(E, listCh.lastNode.getNext().getData());
 	}
+
 	@Test
 	public void removeTest() throws IndexOutOfBoundsException {
-		fail("Not yet implemented");
+
+		LinkedList<Character> listCh = new LinkedList<>();
+
+		listCh.addFirst('A');
+		listCh.addFirst('B');
+		listCh.addFirst('C');
+		listCh.addFirst('D');
+		listCh.addFirst('D');
+
+
+		listCh.remove(1);
+		listCh.remove(2);
+		
+		listCh.print();
+		
+		Character C = 'C';
+		Character A = 'A';
+
+		assertEquals(C, listCh.firstNode.getData());
+		assertEquals(A, listCh.lastNode.getData());
 	}
 
 	@Test
 	public void printTest() {
-		
+
 		LinkedList<String> list = new LinkedList<>();
-		
+
 		list.addLast("John");
 		list.addLast("Yenna");
 		list.addLast("Maria");
 		list.addLast("Will");
-	
+
 //		list.print();
-		
-		
+
 	}
 
 }
